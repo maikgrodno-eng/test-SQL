b27eb72579840c700765ff2339cbddd60543e167
BabichMihail <maikgrodno@gmail.com>
2025-11-16 14:24:49 +0300

    Add files via upload

---
+++ fridge_202511161422.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.fridge (shelves_count,model,color,price,exp_years) VALUES
+	 (5,'samsung r32','silver',2999.99,3),
+	 (6,'lg l51','silver',3150.00,4),
+	 (7,'bosh 153','black',3500.00,3),
+	 (3,'minsk','blue',1999.99,4),
+	 (5,'samsung e56','white',2799.99,4),
+	 (4,'atlant','green',2159.99,3);
